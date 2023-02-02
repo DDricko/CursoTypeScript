3962c105cc7992c14cfdc061305510decad8f8d7
Rodrigo Nascimento <rodrigowaltmann.n@gmail.com>
2023-02-02 12:14:13 -0300

    Utilizando o tipo object

---
+++ src/Aula004_tipo_objects/Aula004_tipo_objects.ts
@@ -0,0 +1,11 @@
+/* eslint-disable*/
+const objetoA: {
+  readonly chaveA: string;
+  chaveB: string;
+} = {
+  chaveA: 'Valor A',
+  chaveB: 'Valor B'
+};
+
+objetoA.chaveB = 'Outro valor';
+console.log(objetoA)
